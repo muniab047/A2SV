8f8171abf588954dea873e37ef8cf7f5c6c633b3
Nasiha Abdella <127668335+muniab047@users.noreply.github.com>
2023-12-27 13:08:18 +0300

    Added solution for Partition Array According to Given Pivot

---
+++ pythontrack/partition-array-according-to-given-pivot.py
@@ -0,0 +1,20 @@
+class Solution:
+    def pivotArray(self, nums: List[int], pivot: int) -> List[int]:
+
+        left=[]
+        middle=[]
+        right=[]
+
+        for i in range(len(nums)):
+            if(nums[i]< pivot):
+                left.append(nums[i])
+
+            elif(nums[i]== pivot):
+                middle.append(nums[i])
+
+            else:
+                right.append(nums[i])
+
+        ans=left+middle+right
+        return ans
+        
